fd23b0687c1c796075431d7ca0a134b53fa4e295
Ekaterina Krutova <1032216536@pfur.ru>
2024-02-24 03:54:51 +0300

    new lab 3

---
+++ labs/lab3/report/lab03.jl
@@ -0,0 +1,41 @@
+using Plots;
+using DifferentialEquations;
+
+function one(du, u, p, t)
+    du[1] = - 0.34*u[1] - 0.93*u[2] + 2*sin(t)
+    du[2] = - 0.54*u[1] - 0.29*u[2] + cos(t) + 3
+end
+
+function two(du, u, p, t)
+    du[1] = - 0.31*u[1] - 0.88*u[2] + 2*sin(2*t)
+    du[2] = - 0.41*(u[1] + 1)*u[2] + cos(t) + 3
+end
+
+const people = Float64[895000, 577000]
+const prom1 = [0.0, 3.0]
+const prom2 = [0.0, 0.0007]
+
+prob1 = ODEProblem(one, people, prom1)
+prob2 = ODEProblem(two, people, prom2)
+
+sol1 = solve(prob1, dtmax=0.1)
+sol2 = solve(prob2, dtmax=0.000001)
+
+A1 = [u[1] for u in sol1.u]
+A2 = [u[2] for u in sol1.u]
+T1 = [t for t in sol1.t]
+A3 = [u[1] for u in sol2.u]
+A4 = [u[2] for u in sol2.u]
+T2 = [t for t in sol2.t]
+
+plt1 = plot(dpi = 300, legend= true, bg =:white)
+plot!(plt1, xlabel="Время", ylabel="Численность", title="Модель боевых действий - случай 1", legend=:outerbottom)
+plot!(plt1, T1, A1, label="Численность армии X", color =:red)
+plot!(plt1, T1, A2, label="Численность армии Y", color =:green)
+savefig(plt1, "lab03_1.png")
+
+plt2 = plot(dpi = 1200, legend= true, bg =:white)
+plot!(plt2, xlabel="Время", ylabel="Численность", title="Модель боевых действий - случай 2", legend=:outerbottom)
+plot!(plt2, T2, A3, label="Численность армии X", color =:red)
+plot!(plt2, T2, A4, label="Численность армии Y", color =:green)
+savefig(plt2, "lab03_2.png")
